062ef7dbb2dc8a1325908eee5b9a50a927c06aca
kajalpatel <k74869672@gmail.com>
2026-03-29 13:15:01 +0530

    Time: 4 ms (41.27%), Space: 43.3 MB (50.39%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,28 +1,26 @@
-import java.util.Stack;
 
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
 
         for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch == '(' || ch == '{' || ch == '[') {
-                stack.push(ch);
-
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-
-                }
-                char top = stack.pop();
-                if ((ch == ')' && top != '(') || (ch == '}' && top != '{') || (ch == ']' && top != '[')) {
-                    return false;
-
+            char cur = s.charAt(i);
+            if (!stack.isEmpty()) {
+                char last = stack.peek();
+                if (isPair(last, cur)) {
+                    stack.pop();
+                    continue;
                 }
             }
-
+            stack.push(cur);
         }
-        return stack.isEmpty();
 
+        return stack.isEmpty();
+    }
+
+    private boolean isPair(char last, char cur) {
+        return (last == '(' && cur == ')') ||
+                (last == '{' && cur == '}') ||
+                (last == '[' && cur == ']');
     }
 }
